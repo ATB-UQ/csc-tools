f37a87215743d7f4adc7a0c5ac610c408d1dff58
Sharif Nada <s.nada@uq.edu.au>
2022-08-02 16:20:40 +1000

    04

---
+++ my_pip_package/math.py
@@ -0,0 +1,9 @@
+from returns import returns
+
+
+@returns(int)
+def div_int(x, y):
+    return x / y
+
+def add(x, y):
+    return x + y
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from my_pip_package import __version__
+
+setup(
+    name='my_pip_package',
+    version=__version__,
+
+    url='https://github.com/MichaelKim0407/tutorial-pip-package',
+    author='Michael Kim',
+    author_email='mkim0407@gmail.com',
+
+    packages=find_packages(),
+
+    install_requires=[
+    'returns-decorator',
+],
+)
