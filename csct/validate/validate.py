73200a55e288229365a95b801a78d59392ae6245
Sharif Nada <s.nada@uq.edu.au>
2022-08-13 12:22:05 +1000

    added metadata schema

--- csct/validate/validate.py
+++ csct/validate/validate.py
@@ -2,21 +2,44 @@ import os
 from pathlib import Path
 import click
 import yaml
-from .schema import raw_metadata_schema
+import cerberus
+
+from .schema import get_metadata_schema
 
 def find_directories(dirs):
     found_dirs = set()
     for dir in dirs:
         found_dirs.update(x[0] for x in os.walk(dir) \
             if (Path(x[0]) / 'atbrepo.yaml').is_file() or \
-                (Path(x[0]) / 'atbrepo.yml').is_file() or \
-                (Path(x[0]) / 'metadata.yaml').is_file() or \
-                (Path(x[0]) / 'metadata.yml').is_file()
+                (Path(x[0]) / 'atbrepo.yml').is_file() #or \
+                #(Path(x[0]) / 'metadata.yaml').is_file() or \
+                #(Path(x[0]) / 'metadata.yml').is_file()
                 )
     return found_dirs
 
-def validate_metadata(dirs, raw_metadata_schema):
-    metadata_schema = yaml.safe_load(raw_metadata_schema)
+def validate_metadata(dirs):
+    click.echo("Validating metadata files in dataset paths...")
+    for dir in dirs:
+        click.echo(f"{dir}          ", nl=False)
+        metadata_path = os.path.join(dir, "atbrepo.yml") #check for this name first
+        if not os.path.exists(metadata_path): #if it's not there...
+            metadata_path = os.path.join(dir, "atbrepo.yaml") #check for the alternative name
+        try: 
+            with open(metadata_path, "r") as c: #try to open the metadata file
+                raw_metadata = yaml.safe_load(c) #if the metadata file is there, create the raw_config dictionary by loading the yaml file
+        except: 
+            click.secho("FAIL", fg='red')
+            raise Exception(f"Could not open metadata file in path: {metadata_path}") #throw exception if it's not there
+    #test_metadata = {'title': '', 'organization': 'mduqf'}
+        validator = cerberus.Validator(get_metadata_schema())
+        validator.validate(raw_metadata)
+
+        if validator.errors:
+            click.secho("FAIL", fg='red')
+            click.secho(f"{validator.errors}", fg='red')
+        else:
+            click.secho("PASS", fg='green')
+
 
 @click.command()
 @click.argument('dirs', nargs=-1, type=click.Path(exists=True, file_okay=False))
@@ -34,4 +57,4 @@ def validate(dirs):
         click.echo("\n".join(dirs))
         raise click.Abort()
 
-    validate_metadata(found_dirs, raw_metadata_schema)    
+    validate_metadata(found_dirs)    
