0b04e440d938cdf02014ea4fd4eb0a9055fbd77a
Sharif Nada <s.nada@uq.edu.au>
2024-04-22 17:17:33 +1000

    metadata command work in progress

--- src/csct/metadata.py
+++ src/csct/metadata.py
@@ -1,14 +1,15 @@
 import click
+import sys
 
 @click.command(short_help="View and edit dataset metadata.")
 @click.argument('dirs', nargs=-1, type=click.Path(exists=True, file_okay=False))
-@click.option('-a', '--all', is_flag=True, help="Validate all dataset properties. [default]")
-@click.option('-e', '--export', is_flag=True, help="Validate all dataset properties and attempt to export dataset.")
-@click.option('-c', '--config', is_flag=True, help="Validate configuration settings.")
-@click.option('-m', '--metadata', is_flag=True, help="Validate configuration settings and dataset metadata.")
-@click.option('-s', '--structure', is_flag=True, help="Validate dataset subdirectory structure.")
-@click.option('-f', '--files', is_flag=True, help="Validate dataset subdirectory structure and files.")
-def metadata(add, remove, replace, view, dirs):
+@click.option('-create', 'action', is_flag=True, flag_value='create', help="Validate all dataset properties. [default]")
+@click.option('-delete', 'action', is_flag=True, flag_value='delete', help="Validate all dataset properties and attempt to export dataset.")
+@click.option('-modify', 'action', is_flag=True, flag_value='modify', help="Validate configuration settings.")
+@click.option('-view', 'action', is_flag=True, flag_value='view', help="Validate configuration settings and dataset metadata.")
+@click.option('-list', 'action', is_flag=True, flag_value='list', help="Validate dataset subdirectory structure.")
+@click.option('-summary', 'action', is_flag=True, flag_value='summary', help="Validate dataset subdirectory structure and files.")
+def metadata(action, dirs):
     """
     View and modify metadata.
 
@@ -16,4 +17,6 @@ def metadata(add, remove, replace, view, dirs):
     DIRS    Directories to recursively scan for datasets. 
             [default: current working directory]
     """
-    pass
+    def info(action):
+        click.echo(getattr(sys.platform, action)())
+    info(action)
