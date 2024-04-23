d52ca4ebd0e9178d6d0314a7fb71214e205ef36c
Sharif Nada <s.nada@uq.edu.au>
2024-04-24 09:53:35 +1000

    metadata changes

--- src/csct/metadata.py
+++ src/csct/metadata.py
@@ -8,11 +8,25 @@ def metadata():
     """
     pass
 
-@click.command(short_help="Add a new value to a metadata field.")
+@click.command(short_help="Add a new metadata field.")
+@click.argument('name', required=True, type=str)
+@click.argument('dirs', nargs=-1, type=click.Path(exists=True, file_okay=False))
+def add(name, value, dirs):
+    """
+    Add a new metadata field.
+
+    \b
+    NAME    The metadata field name to be created.
+    DIRS    Directories to recursively scan for datasets. 
+            [default: current working directory]
+    """
+    print(name, value, dirs)
+
+@click.command(short_help="Add a value to an existing metadata field.")
 @click.argument('name', required=True, type=str)
 @click.argument('value', required=True, type=str)
 @click.argument('dirs', nargs=-1, type=click.Path(exists=True, file_okay=False))
-def add(name, value, dirs):
+def assign(name, value, dirs):
     """
     Assign a new value to a metadata field.  For fields that only accept a single value, the field must not already have a value assigned.  For fields that accept a list of values, the value supplied will be appended to the existing list of values.  
 
@@ -24,14 +38,14 @@ def add(name, value, dirs):
     """
     print(name, value, dirs)
 
-@click.command(short_help="Modify the value of a metadata field.")
+@click.command(short_help="Modify an existing metadata value.")
 @click.argument('name', required=True, type=str)
 @click.argument('old', required=True, type=str)
 @click.argument('new', required=True, type=str)
 @click.argument('dirs', nargs=-1, type=click.Path(exists=True, file_okay=False))
 def modify(name, value, dirs):
     """
-    Modify the value of a metadata field.
+    Modify the value of an existing metadata field.
 
     \b
     NAME    The metadata field to be modified.
@@ -42,13 +56,14 @@ def modify(name, value, dirs):
     """
     pass
 
-@click.command(short_help="Remove an existing metadata field value.")
+@click.command(short_help="Remove existing metadata values or fields. ")
 @click.argument('name', required=True, nargs=1)
+@click.option('-r', '--remove-field', is_flag=True, help="If specified, the metadata field itself will also be removed.")
 @click.option('-v', '--value', multiple=True, type=str, help="Specify a specific value to be removed.  May be used multiple times to specify multiple values.")
 @click.argument('dirs', nargs=-1, type=click.Path(exists=True, file_okay=False))
 def remove(name, value, dirs):
     """
-    Remove existing metadata field values.  
+    Remove existing metadata values or fields.  
     
     If --value is not specified, all values associated with the field name will be removed.
 
@@ -59,8 +74,26 @@ def remove(name, value, dirs):
     """
     pass
 
+@click.command(short_help="Rename an existing metadata field, leaving its values intact.")
+@click.argument('name', required=True, type=str)
+@click.argument('old', required=True, type=str)
+@click.argument('new', required=True, type=str)
+@click.argument('dirs', nargs=-1, type=click.Path(exists=True, file_okay=False))
+def rename(name, value, dirs):
+    """
+    Rename an existing metadata field, leaving its values intact.
+
+    \b
+    OLD     The metadata field name to be replaced.
+    NEW     The new metadata field name.
+    DIRS    Directories to recursively scan for datasets. 
+            [default: current working directory]
+    """
+    pass
 
 
 metadata.add_command(add)
+metadata.add_command(assign)
 metadata.add_command(modify)
 metadata.add_command(remove)
+metadata.add_command(rename)
